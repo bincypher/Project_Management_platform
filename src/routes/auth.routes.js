a8eae35915f6022ba1c04cb20bf8577faf4b0947
bincypher <binayak.bidyasagar99@gmail.com>
2026-05-28 11:35:28 +0530

    adding the logout functionality

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import {login, registerUser } from "../controllers/auth.controllers.js";
+import {login, registerUser,logoutUser } from "../controllers/auth.controllers.js";
 import { validate } from "../middlewares/validator.middlewares.js";
 import { userRegisterValidator,userloginValidator } from "../validators/index.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+
 
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(userloginValidator(), validate, login);
+
+//secure routes
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
